bc60396abc872fb66e691c2a2de68313f19b6819
rinkuuu <backupfb2032@gmail.com>
2021-02-06 17:34:58 +0545

    cockpit compoenent done

--- src/components/Cockpit.js
+++ src/components/Cockpit.js
@@ -1,10 +1,62 @@
+import { useEffect, useState } from "react";
+import "../css/Cockpit.css";
+
 const Cockpit = () => {
   const products = [
     { productName: "Iphone12", productImage: "https://bit.ly/39VLnZC" },
     { productName: "Google Pixel", productImage: "https://bit.ly/2YTjEmh" },
     { productName: "Lenovo k9", productImage: "https://lnv.gy/3oVhHAr" },
   ];
-  return <div className="cockpit"></div>;
+  const [activeProductIndex, setActiveProductIndex] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setActiveProductIndex((prev) => (prev === 2 ? 0 : prev + 1));
+    }, 5000);
+  }, []);
+
+  return (
+    <div className="cockpit">
+      <main>
+        <section>
+          <img
+            src={products[activeProductIndex].productImage}
+            alt="product-name"
+          />
+        </section>
+        <section>
+          <h1>{products[activeProductIndex].productName}</h1>
+          <button>See What's init</button>
+        </section>
+      </main>
+      <section className="toggler">
+        <button
+          style={
+            activeProductIndex === 0
+              ? { background: "rgba(0, 0, 0, 0.5)" }
+              : null
+          }
+          onClick={() => setActiveProductIndex(0)}
+        ></button>
+        <button
+          style={
+            activeProductIndex === 1
+              ? { background: "rgba(0, 0, 0, 0.5)" }
+              : null
+          }
+          onClick={() => setActiveProductIndex(1)}
+        ></button>
+        <button
+          style={
+            activeProductIndex === 2
+              ? { background: "rgba(0, 0, 0, 0.5)" }
+              : null
+          }
+          onClick={() => setActiveProductIndex(2)}
+        ></button>
+      </section>{" "}
+    </div>
+  );
 };
 
 export default Cockpit;
