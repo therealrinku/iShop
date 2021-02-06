80784f5cba23182299480d6f07ac1800d1a4ff16
rinkuuu <backupfb2032@gmail.com>
2021-02-06 15:29:27 +0545

    products for cockpit added

--- src/components/Cockpit.js
+++ src/components/Cockpit.js
@@ -1,4 +1,9 @@
 const Cockpit = () => {
+  const products = [
+    { productName: "Iphone12", productImage: "https://bit.ly/39VLnZC" },
+    { productName: "Google Pixel", productImage: "https://bit.ly/2YTjEmh" },
+    { productName: "Lenovo k9", productImage: "https://lnv.gy/3oVhHAr" },
+  ];
   return <div className="cockpit"></div>;
 };
 
