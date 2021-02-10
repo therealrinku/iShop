616d2c66c097f1c97f2bf142a17f429eb920985c
rinkuuu <backupfb2032@gmail.com>
2021-02-10 08:17:49 +0545

    product short description added

--- src/components/Cockpit.js
+++ src/components/Cockpit.js
@@ -3,9 +3,26 @@ import "../css/Cockpit.css";
 
 const Cockpit = () => {
   const products = [
-    { productName: "Iphone12", productImage: "https://bit.ly/39VLnZC" },
-    { productName: "Google Pixel", productImage: "https://bit.ly/2YTjEmh" },
-    { productName: "Lenovo k9", productImage: "https://lnv.gy/3oVhHAr" },
+    {
+      productName: "Iphone12",
+      productImage: "https://bit.ly/39VLnZC",
+      productShortDescription: `A14 Bionic, the fastest chip in a smartphone.
+    An edge-to-edge OLED display. Ceramic Shield with four times better drop performance. 
+    And Night mode on every camera. `,
+    },
+    {
+      productName: "Google Pixel",
+      productImage: "https://bit.ly/2YTjEmh",
+      productShortDescription: `The Qualcomm Snapdragon 730G processor and 6 gigs of RAM inside deliver 
+      impressive performance with very few stutters, even when gaming, the 3,140-mAh 
+      battery capacity easily lasts a full day.`,
+    },
+    {
+      productName: "Lenovo k9",
+      productImage: "https://lnv.gy/3oVhHAr",
+      productShortDescription: `A battery that lasts full day,
+    a camera that save your amazing memories and a RAM that saves your day.`,
+    },
   ];
   const [activeProductIndex, setActiveProductIndex] = useState(0);
 
@@ -26,6 +43,7 @@ const Cockpit = () => {
         </section>
         <section>
           <h1>{products[activeProductIndex].productName}</h1>
+          <p>{products[activeProductIndex].productShortDescription}</p>
           <button>See What's init</button>
         </section>
       </main>
