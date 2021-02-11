7ec45d4c03dac22d180305f52095407885a49ffa
rinkuuu <backupfb2032@gmail.com>
2021-02-11 07:11:06 +0545

    product title updated to link

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import "../css/Product.css";
 
 const Product = ({ productImage, productName, productPrice }) => {
   return (
     <div className="product">
       <img src={productImage} alt={productName} />
-      <h4>{productName}</h4>
+      <Link to="/">{productName}</Link>
       <p>${productPrice}</p>
     </div>
   );
