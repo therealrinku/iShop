25ae60e7b1d31444d15e4e88656614c3ea6505cd
rinkuuu <backupfb2032@gmail.com>
2021-02-12 12:36:32 +0545

    explore page update

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,11 +1,45 @@
 import Navbar from "./Navbar";
+import * as productsActions from "../redux/products/productsActions";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+import Product from "./Product";
 
-const AllProducts = () => {
+const AllProducts = ({ products, loading, error, LOAD_PRODUCTS }) => {
+  useEffect(() => {
+    if (products.length < 1) {
+      LOAD_PRODUCTS();
+    }
+  }, []);
+
   return (
     <div className="all--products">
       <Navbar />
+      {products.map((product, i) => {
+        return (
+          <Product
+            key={i}
+            productImage={product.productImage}
+            productName={product.productName}
+            productPrice={product.productPrice}
+          />
+        );
+      })}
     </div>
   );
 };
 
-export default AllProducts;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products.products,
+    loading: state.products.loading,
+    error: state.products.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    LOAD_PRODUCTS: () => dispatch(productsActions.LOAD_PRODUCTS),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts);
