942a01462fe8748fa1fa6268b86adb926c021c22
rinkuuu <backupfb2032@gmail.com>
2021-02-12 09:31:10 +0545

    firebase setup done

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQD3xrFb_LP_FhIs_wyNmUcRW-8YJ_P-E",
+  authDomain: "rinkshoop.firebaseapp.com",
+  projectId: "rinkshoop",
+  storageBucket: "rinkshoop.appspot.com",
+  messagingSenderId: "1008214467743",
+  appId: "1:1008214467743:web:652dbc94f03598bd142eaa",
+  measurementId: "G-8DLQ84GG1T",
+};
+
+export default firebaseConfig;
---
+++ src/firebase/db.js
@@ -0,0 +1,8 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+import config from "./config";
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+export default db;
