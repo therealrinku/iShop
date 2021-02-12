b50eff4b21c896fa729ea0787cff25db9b8d00c7
rinkuuu <backupfb2032@gmail.com>
2021-02-12 12:29:07 +0545

    all products route update

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,5 +1,11 @@
+import Navbar from "./Navbar";
+
 const AllProducts = () => {
-  return <div className="all--products"></div>;
+  return (
+    <div className="all--products">
+      <Navbar />
+    </div>
+  );
 };
 
 export default AllProducts;
