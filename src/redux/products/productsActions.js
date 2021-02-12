e0f9a13d7cd5de63ba828acf3c936d70f7e6a6ec
rinkuuu <backupfb2032@gmail.com>
2021-02-12 12:27:55 +0545

    load products action updated

--- src/redux/products/productsActions.js
+++ src/redux/products/productsActions.js
@@ -1,6 +1,15 @@
 import productActionTypes from "./productsActionTypes";
+import getProducts from "../../functions/getProducts";
 
-export const LOAD_PRODUCTS = () => (dispatch) => {
-  dispatch({ type: productActionTypes.LOADING_PRODUCTS });
-  //load
+export const LOAD_PRODUCTS = () => async (dispatch) => {
+  try {
+    dispatch({ type: productActionTypes.LOADING_PRODUCTS });
+    const products = await getProducts();
+    dispatch({ type: productActionTypes.LOAD_PRODUCTS, payload: products });
+  } catch (err) {
+    dispatch({
+      type: productActionTypes.SOMETHING_WENT_FISHY,
+      payload: err.message,
+    });
+  }
 };
