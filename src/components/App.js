b38609e90f159bc9b3477e6c99d0ff599e626cbc
rinkuuu <backupfb2032@gmail.com>
2021-02-12 12:30:04 +0545

    explore route registered

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import ProductDetails from "./ProductDetails";
+import AllProducts from "./AllProducts";
 import "../css/App.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Route path="/" exact component={Homepage} />
       <Route path="/product/:product_id" exact component={ProductDetails} />
+      <Route path="/explore" exact component={AllProducts} />
     </BrowserRouter>
   );
 };
