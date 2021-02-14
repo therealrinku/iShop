07b9ffc501c6d17f164102f5533c6ff0db9929b6
rinkuuu <backupfb2032@gmail.com>
2021-02-14 13:00:35 +0545

    mobile search bar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,45 @@
 import { NavLink } from "react-router-dom";
-import { HiOutlineShoppingBag, FiUser, HiOutlineSearch } from "react-icons/all";
+import {
+  HiOutlineShoppingBag,
+  FiUser,
+  HiOutlineSearch,
+  BiX,
+} from "react-icons/all";
+import { useState } from "react";
 import "../css/Navbar.css";
 
 const Navbar = () => {
+  const [showMobileSearchBar, setShowMobileSearchBar] = useState(false);
+
   return (
     <nav className="nav--bar">
-      <section>
+      <section style={showMobileSearchBar ? { display: "none" } : null}>
         <NavLink to="/">iShop</NavLink>
       </section>
 
-      <section>
+      <section style={showMobileSearchBar ? { display: "none" } : null}>
         <input type="text" placeholder="What are you looking for?" />
       </section>
 
-      <section>
-        <button className="mobile-search-btn">
+      <div
+        className="mobile-search-bar"
+        style={!showMobileSearchBar ? { display: "none" } : null}
+      >
+        <input type="text" placeholder="What are you looking for?" />
+        <button
+          className="cancel-btn"
+          style={!showMobileSearchBar ? { display: "none" } : null}
+          onClick={() => setShowMobileSearchBar(false)}
+        >
+          <BiX />
+        </button>
+      </div>
+
+      <section style={showMobileSearchBar ? { display: "none" } : null}>
+        <button
+          className="mobile-search-btn"
+          onClick={() => setShowMobileSearchBar(true)}
+        >
           <HiOutlineSearch />
           <p>Search</p>
         </button>
