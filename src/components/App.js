f4389d98b43ffbc1884f1accd25247659b6230fd
rinkuuu <backupfb2032@gmail.com>
2021-02-14 11:27:57 +0545

    deleted all components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,5 @@
-import { BrowserRouter, Route } from "react-router-dom";
-import Homepage from "./Homepage";
-import ProductDetails from "./ProductDetails";
-import AllProducts from "./AllProducts";
-import "../css/App.css";
-import Cart from "./Cart";
-
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Route path="/" exact component={Homepage} />
-      <Route path="/product/:product_id" exact component={ProductDetails} />
-      <Route path="/explore" exact component={AllProducts} />
-      <Route path="/cart" exact component={Cart} />
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 };
 
 export default App;
