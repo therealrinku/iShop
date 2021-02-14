5e62c223a1c13e3ee2c5e9606faee6709d4ea848
rinkuuu <backupfb2032@gmail.com>
2021-02-14 13:15:50 +0545

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,23 @@
 import { NavLink } from "react-router-dom";
-import {
-  HiOutlineShoppingBag,
-  FiUser,
-  HiOutlineSearch,
-  BiX,
-} from "react-icons/all";
-import { useState } from "react";
+import { HiOutlineShoppingBag, FiUser, HiOutlineSearch } from "react-icons/all";
 import "../css/Navbar.css";
 
 const Navbar = () => {
-  const [showMobileSearchBar, setShowMobileSearchBar] = useState(false);
-
   return (
     <nav className="nav--bar">
-      <section style={showMobileSearchBar ? { display: "none" } : null}>
+      <section>
         <NavLink to="/">iShop</NavLink>
       </section>
 
-      <section style={showMobileSearchBar ? { display: "none" } : null}>
-        <input type="text" placeholder="What are you looking for?" />
-      </section>
-
-      <div
-        className="mobile-search-bar"
-        style={!showMobileSearchBar ? { display: "none" } : null}
-      >
-        <input type="text" placeholder="What are you looking for?" />
-        <button
-          className="cancel-btn"
-          style={!showMobileSearchBar ? { display: "none" } : null}
-          onClick={() => setShowMobileSearchBar(false)}
-        >
-          <BiX />
-        </button>
-      </div>
-
-      <section style={showMobileSearchBar ? { display: "none" } : null}>
-        <button
-          className="mobile-search-btn"
-          onClick={() => setShowMobileSearchBar(true)}
-        >
+      <section>
+        <NavLink to="/search">
           <HiOutlineSearch />
-          <p>Search</p>
-        </button>
+        </NavLink>
         <NavLink to="/login">
           <FiUser />
-          <p>Login</p>
         </NavLink>
         <NavLink to="/bag">
           <HiOutlineShoppingBag />
-          <p>Bag</p>
         </NavLink>
       </section>
     </nav>
