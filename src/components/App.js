b04b0c2aafcdd3b5afdf3ac50bbeaac3fb0c1599
rinkuuu <backupfb2032@gmail.com>
2021-02-15 08:04:53 +0545

    item details page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import ExplorePage from "../pages/ExplorePage";
 import Homepage from "../pages/Homepage";
+import ItemDetailsPage from "../pages/ItemDetailsPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Homepage} />
       <Route path="/explore" exact component={ExplorePage} />
+      <Route path="/product/:productId" exact component={ItemDetailsPage} />
     </BrowserRouter>
   );
 };
---
+++ src/pages/ItemDetailsPage.js
@@ -0,0 +1,36 @@
+import { connect } from "react-redux";
+import Navbar from "../components/Navbar";
+import { useParams } from "react-router-dom";
+
+const ItemDetailsPage = ({ products }) => {
+  const params = useParams();
+  const product = products.filter((p) => p.productId === params.productId)[0];
+
+  return (
+    <div className="item--details--page">
+      <Navbar />
+      <div>
+        <section>
+          <img src={product.productImageURL} alt={`${product.productName}`} />
+        </section>
+
+        <section>
+          <h4>{product.productName}</h4>
+          <p>{product.productPrice}</p>
+          {product.productSpecs.map((spec, i) => {
+            return <li key={i}>{spec}</li>;
+          })}
+          <button>Add to Bag</button>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.products.products,
+  };
+};
+
+export default connect(mapStateToProps)(ItemDetailsPage);
