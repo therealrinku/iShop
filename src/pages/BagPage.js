5530a5a8c07280d49331b8c70ce5bb4351601128
rinkuuu <backupfb2032@gmail.com>
2021-02-15 13:15:37 +0545

    back to shooping btn if cart is empty

--- src/pages/BagPage.js
+++ src/pages/BagPage.js
@@ -1,27 +1,42 @@
 import { connect } from "react-redux";
 import CartItem from "../components/CartItem";
 import Navbar from "../components/Navbar";
+import { BsArrowRight } from "react-icons/all";
+import { useHistory } from "react-router-dom";
 import "../css/BagPage.css";
 
 const BagPage = ({ cartItems }) => {
+  const history = useHistory();
+
   return (
     <div className="bag--page">
       <Navbar />
 
       <main>
         <h4>My Bag</h4>
-        <section className="cart-items">
-          {cartItems.map((item) => {
-            return (
-              <CartItem
-                itemImageURL={item.productImageURL}
-                itemName={item.productName}
-                itemPrice={item.productPrice}
-                itemQuantity={item.productQuantity}
-              />
-            );
-          })}
-        </section>
+
+        {cartItems.length >= 1 ? (
+          <section className="cart-items">
+            {cartItems.map((item) => {
+              return (
+                <CartItem
+                  itemImageURL={item.productImageURL}
+                  itemName={item.productName}
+                  itemPrice={item.productPrice}
+                  itemQuantity={item.productQuantity}
+                />
+              );
+            })}
+          </section>
+        ) : (
+          <section className="back-to-shopping">
+            <p>Your Shopping Cart is Empty.</p>
+            <button onClick={() => history.push("/explore")}>
+              <p>Get Back to Shopping</p>
+              <BsArrowRight />
+            </button>
+          </section>
+        )}
       </main>
     </div>
   );
