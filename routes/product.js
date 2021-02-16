828f5c90aa7af21409086bca81444697d108f0d3
rinkuuu <backupfb2032@gmail.com>
2021-02-16 16:19:27 +0545

    dummy lines deleted

--- routes/product.js
+++ routes/product.js
@@ -29,8 +29,6 @@ router.post("/create", (req, res) => {
     is_latest,
   } = req.body;
 
-  console.log(product_price);
-
   db.query(
     `INSERT INTO products(product_name,product_image_url,product_specs,product_price,is_hot,is_latest) 
     VALUES
@@ -42,7 +40,7 @@ router.post("/create", (req, res) => {
     '${is_latest || false}')`,
     (err, res1) => {
       if (!err) res.send("success");
-      else console.log(err);
+      else res.send(err);
     }
   );
 });
