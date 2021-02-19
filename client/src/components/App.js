8375532e5677c77b550722fc6360252692798df4
rinkuuu <backupfb2032@gmail.com>
2021-02-19 08:05:54 +0545

    login with token setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,19 @@ import Homepage from "../pages/Homepage";
 import ItemDetailsPage from "../pages/ItemDetailsPage";
 import LoginPage from "../pages/LoginPage";
 import ProfilePage from "../pages/ProfilePage";
+import * as userActions from "../redux/user/userActions";
+import { connect } from "react-redux";
+import { useEffect } from "react";
 
-const App = () => {
+const App = ({ LOGIN_WITH_TOKEN }) => {
+  const login_token = localStorage.getItem("login_token");
+
+  useEffect(() => {
+    if (login_token) {
+      LOGIN_WITH_TOKEN(login_token);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Route path="/" exact component={Homepage} />
@@ -19,4 +30,10 @@ const App = () => {
   );
 };
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    LOGIN_WITH_TOKEN: (token) => dispatch(userActions.LOGIN_WITH_TOKEN(token)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
