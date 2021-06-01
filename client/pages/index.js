60f658eb760612292d5cac863b22516ad4fc7e7f
therealrinku <backupfb2032@gmail.com>
2021-06-01 17:43:29 +0545

    hero section done

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,20 @@
 import styles from "../styles/LandingPage.module.css";
+import HeroImage from "../assets/hero.jpg";
 
 export default function LandingPage() {
-  return <main className={styles.container}></main>;
+  return (
+    <main className={styles.container}>
+      <section className={styles.heroSection}>
+        <div>
+          <img src={HeroImage} alt="new_product_image" />
+        </div>
+
+        <div>
+          <u>ONE LOCATION ALL SOLUTION</u>
+          <p>Get all the latest and hottest smartphones in one place, mobyoxx.</p>
+          <button>Shop Now</button>
+        </div>
+      </section>
+    </main>
+  );
 }
