13d779f89c8cfc8ab278a2e1499c7f3c779a4d1d
therealrinku <backupfb2032@gmail.com>
2021-06-01 21:38:24 +0545

    slider created

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "14d00a557e15054e4c8d";
+/******/ 	var hotCurrentHash = "12eb708cf06bae0915b6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,13 @@
 import styles from "../styles/LandingPage.module.css";
 import HeroImage from "../assets/hero.jpg";
 import Item from "../components/Item";
+import { MdChevronRight, MdChevronLeft } from "react-icons/md";
+import { useState } from "react";
 
 export default function LandingPage({ hottestProducts }) {
+  const [visibleHottestItems, setVisibleHottestItems] = useState(hottestProducts);
+  const [slideIndex, setSlideIndex] = useState(0);
+
   return (
     <main className={styles.container}>
       <section className={styles.heroSection}>
@@ -18,9 +23,13 @@ export default function LandingPage({ hottestProducts }) {
       </section>
 
       <section className={styles.hottestSection}>
-        <u>HOTTEST PRODUCTS</u>
+        <u>HOT PRODUCTS</u>
+
         <article>
-          {hottestProducts.map((product) => {
+          <button onClick={() => setSlideIndex((prev) => prev - 1)} disabled={slideIndex === 0}>
+            <MdChevronLeft />
+          </button>
+          {visibleHottestItems.slice(slideIndex).map((product) => {
             return (
               <Item
                 itemId={product.productId}
@@ -31,6 +40,9 @@ export default function LandingPage({ hottestProducts }) {
               />
             );
           })}
+          <button onClick={() => setSlideIndex((prev) => prev + 1)} disabled={slideIndex === 6}>
+            <MdChevronRight />
+          </button>
         </article>
       </section>
     </main>
@@ -40,12 +52,12 @@ export default function LandingPage({ hottestProducts }) {
 LandingPage.defaultProps = {
   hottestProducts: [
     { productName: "Iphone 12", productImageURL: "https://bit.ly/3wF70WS", productId: 1, price: 1299 },
-    { productName: "Samsung Galaxy S9", productImageURL: "https://bit.ly/34zeVc3", productId: 2, price: 999 },
-    { productName: "Iphone X", productImageURL: "https://bit.ly/3vI7jQu", productId: 3, price: 799 },
-    { productName: "Pixel 2", productImageURL: "https://bit.ly/34zs6dl", productId: 4, price: 899 },
-    { productName: "Huawei Mate 20 Lite", productImageURL: "https://bit.ly/3uDiNmX", productId: 5, price: 1199 },
-    { productName: "ZenPhone 3", productImageURL: "https://bit.ly/3g4OUad", productId: 6, price: 499 },
-    { productName: "Sony Ericsson XC12", productImageURL: "https://bit.ly/3g6daJ4", productId: 7, price: 399 },
-    { productName: "Vivo V5", productImageURL: "https://bit.ly/3p9Q7AV", productId: 8, price: 299 },
+    { productName: "Samsung Galaxy S9", productImageURL: "https://bit.ly/3wF70WS", productId: 2, price: 999 },
+    { productName: "Iphone X", productImageURL: "https://bit.ly/3wF70WS", productId: 3, price: 799 },
+    { productName: "Pixel 2", productImageURL: "https://bit.ly/3wF70WS", productId: 4, price: 899 },
+    { productName: "Huawei Mate 20 Lite", productImageURL: "https://bit.ly/3wF70WS", productId: 5, price: 1199 },
+    { productName: "ZenPhone 3", productImageURL: "https://bit.ly/3wF70WS", productId: 6, price: 499 },
+    { productName: "Sony Ericsson XC12", productImageURL: "https://bit.ly/3wF70WS", productId: 7, price: 399 },
+    { productName: "Vivo V5", productImageURL: "https://bit.ly/3wF70WS", productId: 8, price: 299 },
   ],
 };
