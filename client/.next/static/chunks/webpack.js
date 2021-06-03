c2c350dbbfa5a2144f245739e1e36070917a737b
therealrinku <backupfb2032@gmail.com>
2021-06-03 10:15:47 +0545

    some tweaks

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2efd8070cc72af6a5c81";
+/******/ 	var hotCurrentHash = "2e3e5510e4dab8ffa002";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
